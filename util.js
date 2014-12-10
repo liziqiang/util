afe85361594a25d59908f56bb484c6faad0a2d31
lizq <lizq@funshion.com>
2014-12-10 10:13:49 +0800

    添加获取滚动高度等方法

--- util.js
+++ util.js
@@ -1,4 +1,4 @@
-(function() {
+(function( win ) {
     var util = {};
     // url解析相关
     util.url = {
@@ -34,6 +34,9 @@
             $des.innerHTML = html;
         }
         return html;
+    };
+    util.replaceAll = function( input, find, replace ) {
+        return input.replace( RegExp( find.replace( /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&" ), 'g' ), replace );
     };
     // 版本号比较
     util.compareVersion = function( vera, verb ) {
@@ -140,6 +143,266 @@
         }
         ele.innerHTML = str + ellipsisText;
         tmp.parentNode.removeChild( tmp );
+    };
+    // 获取浏览器滚动条宽高
+    util.detectScrollbarWidthHeight = function() {
+        var $div = document.createElement( 'div' );
+        $div.id = '__detect__';
+        $div.style.overflow = 'scroll';
+        $div.style.visibility = 'hidden';
+        $div.style.position = 'absolute';
+        $div.style.width = '100px';
+        $div.style.height = '100px';
+        document.body.appendChild( $div );
+        var hw = {
+            width  : $div.offsetWidth - $div.clientWidth,
+            height : $div.offsetHeight - $div.clientHeight
+        };
+        $div.parentNode.removeChild( $div );
+        return hw;
+    };
+    // 获取页面滚动高度
+    util.getScrollTop = function() {
+        // 如果以声明DTD，则使用documentElement，但是Chrome是例外
+        var doc = document, client = doc.compatMode == 'CSS1Compat' ? doc.documentElement : doc.body;
+        return client.scrollTop;
+    };
+    // 获取页面可视高度，不包含滚动条
+    util.getClientHeight = function() {
+        // 如果以声明DTD，则使用documentElement，但是Chrome是例外
+        var doc = document, client = doc.compatMode == 'CSS1Compat' ? doc.documentElement : doc.body;
+        return client.clientHeight;
+    };
+    // 获取页面总体高估
+    util.getScrollHeight = function() {
+        // 如果以声明DTD，则使用documentElement，但是Chrome是例外
+        var doc = document, client = doc.compatMode == 'CSS1Compat' ? doc.documentElement : doc.body;
+        return Math.max( client.scrollHeight, client.clientHeight );
+    };
+    // 检测节点是否包含另一节点
+    util.contains = function( pNode, cNode ) {
+        if ( typeof pNode.contains == 'function' && (!util.browser.engine.webkit || util.browser.engine.webkit >= 522) ) {
+            return pNode.contains( cNode );
+        } else if ( typeof pNode.compareDocumentPosition == 'function' ) {
+            return !!(pNode.compareDocumentPosition( cNode ) & 16);
+        } else {
+            var node = cNode.parentNode;
+            do {
+                if ( node == pNode ) {
+                    return true;
+                } else {
+                    node = node.parentNode;
+                }
+            } while ( node !== null );
+            return false;
+        }
+    };
+    // 浏览器相关
+    util.browser = function() {
+        // 呈现引擎
+        var engine = {
+            ie     : 0,
+            gecko  : 0,
+            webkit : 0,
+            khtml  : 0,
+            opera  : 0,
+            // 完整的版本号
+            ver    : null
+        };
+        // 浏览器
+        var browser = {
+            ie      : 0,
+            firefox : 0,
+            safari  : 0,
+            konq    : 0,
+            opera   : 0,
+            chrome  : 0,
+            // 完整的版本号
+            ver     : null
+        };
+        // 平台、设备和操作系统
+        var system = {
+            win       : false,
+            mac       : false,
+            x11       : false,
+            // 移动设备
+            iPhone    : false,
+            iPod      : false,
+            iPad      : false,
+            iOS       : false,
+            android   : false,
+            nokiaN    : false,
+            winMobile : false,
+            // 游戏系统
+            wii       : false,
+            ps        : false
+        };
+        // 检测呈现引擎和浏览器
+        var ua = navigator.userAgent;
+        // opera
+        if ( window.opera ) {
+            engine.ver = browser.ver = window.opera.version();
+            engine.opera = browser.opera = parseFloat( engine.ver );
+        } else if ( /AppleWebkit\/(\S+)/.test( ua ) ) {
+            engine.ver = RegExp.$1;
+            engine.webkit = parseFloat( engine.ver );
+            // 确定是Chrome还是Safari
+            if ( /Chrome\/(\S+)/.test( ua ) ) {
+                browser.ver = RegExp.$1;
+                browser.chrome = parseFloat( browser.ver );
+            } else if ( /Version\/(\S+)/.test( ua ) ) {
+                // 此方式只适合Safari3及更高版本
+                browser.ver = RegExp.$1;
+                browser.safari = parseFloat( browser.ver );
+            } else {
+                // Safari低版本处理
+                var safariVersion = 1;
+                if ( engine.webkit < 100 ) {
+                    safariVersion = 1;
+                } else if ( engine.webkit < 312 ) {
+                    safariVersion = 1.2;
+                } else if ( engine.webkit < 412 ) {
+                    safariVersion = 1.3;
+                } else {
+                    safariVersion = 2;
+                }
+                browser.safari = browser.ver = safariVersion;
+            }
+        } else if ( /KHTML\/(\S+)/.test( ua ) || /Konqueror\/([^;]+)/.test( ua ) ) {
+            engine.ver = browser.ver = RegExp.$1;
+            engine.khtml = browser.konq = parseFloat( engine.ver );
+        } else if ( /rv:([^\)]+)\) Gecko\/\d{8}/.test( ua ) ) {
+            engine.ver = RegExp.$1;
+            engine.gecko = parseFloat( engine.ver );
+            // 确定是不是Firefox
+            if ( /Firefox\/(\S+)/.test( ua ) ) {
+                browser.ver = RegExp.$1;
+                browser.firefox = parseFloat( browser.ver );
+            }
+        } else if ( /MSIE ([^;]+)/.test( ua ) ) {
+            engine.ver = browser.ver = RegExp.$1;
+            engine.ie = browser.ie = parseFloat( engine.ver );
+        }
+        // 检测浏览器
+        browser.ie = engine.ie;
+        browser.opera = engine.opera;
+        // 检测平台
+        var p = navigator.platform;
+        system.win = p.indexOf( 'Win' ) == 0;
+        system.mac = p.indexOf( 'Mac' ) == 0;
+        system.x11 = (p.indexOf( 'X11' ) == 0 || p.indexOf( 'Linux' ) == 0);
+        // 检测Windows操作系统
+        if ( system.win ) {
+            if ( /Win(?:dows )?([^do]{2})\s?(\d+\.\d+)?/.test( ua ) ) {
+                if ( RegExp.$1 == 'NT' ) {
+                    switch ( RegExp.$2 ) {
+                        case '5.0':
+                            system.win = '2000';
+                            break;
+                        case '5.1':
+                            system.win = 'XP';
+                            break;
+                        case '6.0':
+                            system.win = 'Vista';
+                            break;
+                        case '6.1':
+                            system.win = '7';
+                            break;
+                        default :
+                            system.win = RegExp.$1;
+                            break;
+                    }
+                } else if ( RegExp.$1 == '9x' ) {
+                    system.win = 'ME';
+                } else {
+                    system.win = RegExp.$1;
+                }
+            }
+        }
+        // 移动设备
+        system.iPhone = ua.indexOf( 'iPhone' ) > -1;
+        system.iPod = ua.indexOf( 'iPod' ) > -1;
+        system.iPad = ua.indexOf( 'iPad' ) > -1;
+        // 检测iOS版本
+        if ( system.mac && ua.indexOf( 'Mobile' ) > -1 ) {
+            if ( /CPU (?:iPhone )?OS (\d+_\d+)/.test( ua ) ) {
+                system.iOS = parseFloat( RegExp.$1.replace( '_', '.' ) );
+            } else {
+                system.iOS = 2;
+            }
+        }
+        // 检测Android版本
+        if ( /Android (\d+\.\d+)/.test( ua ) ) {
+            system.android = parseFloat( RegExp.$1 );
+        }
+        system.nokiaN = ua.indexOf( 'NokiaN' ) > -1;
+        // winPhone
+        if ( system.win == 'CE' ) {
+            system.winMobile = system.win;
+        } else if ( system.win == 'Ph' ) {
+            if ( /Windows Phone OS (\d+.\d+)/.test( ua ) ) {
+                system.win = 'Phone';
+                system.winMobile = parseFloat( RegExp.$1 );
+            }
+        }
+        system.wii = ua.indexOf( 'Wii' ) > -1;
+        system.ps = /playstation/i.test( ua );
+        return {
+            engine  : engine,
+            browser : browser,
+            system  : system
+        };
+    }();
+    // 加载CSS
+    util.loadStyles = function( url ) {
+        var link = document.createElement( 'link' );
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        var head = document.getElementsByTagName( 'head' )[ 0 ];
+        head.appendChild( link );
+    };
+    util.loadScriptString = function( code ) {
+        var script = document.createElement( 'script' );
+        script.type = 'text/javascript';
+        try {
+            script.appendChild( document.createTextNode( code ) );
+        } catch ( ex ) {
+            script.text = code;
+        }
+        document.body.appendChild( script );
+    };
+    util.loadStyleString = function( css ) {
+        var style = document.createElement( 'style' );
+        style.type = 'text/css';
+        try {
+            style.appendChild( document.createTextNode( css ) );
+        } catch ( ex ) {
+            style.styleSheet.cssText = css;
+        }
+        var head = document.getElementsByTagName( 'head' )[ 0 ];
+        head.appendChild( style );
+    };
+    // 获取选中的文本
+    util.getSelectedText = function( textbox ) {
+        if ( typeof textbox.selectionStart == 'number' ) {
+            return textbox.value.substring( textbox.selectionStart, textbox.selectionEnd );
+        } else if ( document.selection ) {
+            return document.selection.createRange().text;
+        }
+    };
+    // 设置文本选中
+    util.selectText = function( textbox, start, end ) {
+        if ( textbox.setSelectionRange ) {
+            textbox.setSelectionRange( start, end );
+        } else if ( textbox.createTextRange ) {
+            var range = textbox.createTextRange();
+            range.collapse( true );
+            range.moveStart( 'character', start );
+            range.moveEnd( 'character', end - start );
+            range.select();
+        }
+        textbox.focus();
     };
     // cookie方法
     util.cookie = {
@@ -287,6 +550,17 @@
                 return event.charCode;
             } else {
                 return event.keyCode;
+            }
+        },
+        getClipboardText : function( event ) {
+            var clipboardData = (event.clipboardData || window.clipboardData);
+            return clipboardData.getData( 'text' );
+        },
+        setClipboardText : function( event, value ) {
+            if ( event.clipboardData ) {
+                return event.clipboardData.setData( 'text/plain', value );
+            } else if ( window.clipboardData ) {
+                return window.clipboardData.setData( 'text', value );
             }
         },
         preventDefault   : function( event ) {
